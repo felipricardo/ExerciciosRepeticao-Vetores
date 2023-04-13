ae57ffbec5a97f69a6def9d99f74ad1afc8e2ca5
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-04-12 21:11:28 -0300

    Update vetores3.py

--- vetores3.py
+++ vetores3.py
@@ -5,3 +5,18 @@ VETORES3 - Faça um programa para corrigir provas de múltipla escolha com somat
 - Para cada aluno seu número e sua nota;
 - A percentagem de aprovação, sabendo-se que a nota mínima é 6,0
 """
+gaba = [0] * 10
+print("Digite o gabarito da prova: ")
+for i in range(10):
+    print("Questao ", i+1, ":")
+    gaba[i] = int(input())
+
+for aluno in range(15):
+    print("aluno: ", aluno+1)
+    nota = 0
+    for questao in range(10):
+        print("resp questao", questao+1,":")
+        resp = int(input())
+        if resp == gaba[questao]:
+            nota += 1
+    print("A nota e :", nota)
