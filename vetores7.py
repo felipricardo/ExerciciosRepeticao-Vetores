22240be5382f3f03bddae0a7951922a505de4010
Felipe Ricardo <85915186+felipricardo@users.noreply.github.com>
2023-04-12 21:35:17 -0300

    Update vetores7.py

--- vetores7.py
+++ vetores7.py
@@ -1,34 +1,55 @@
 '''
-Faça um programa para corrigir provas de 
-múltipla escolha com somatória. Cada prova 
-tem dez questões e cada questão vale 1 ponto. 
-O primeiro conjunto de dados a ser lido é o 
-gabarito da prova. Os outros dados serão os números 
-dos alunos e sua respectivas respostas. Existem 15 
-alunos matriculados. Calcule e mostre:
-- Para cada aluno seu número e sua nota;
-- A percentagem de aprovação, sabendo-se que a nota mínima é 6,0
-'''
+Uma empresa possui ônibus com 48 lugares 
+(24 nas janelas e 24 no corredor). Faça um programa
+ que utilize dois vetores para controlar as poltronas
+ ocupadas no corredor e na janela. Considere
+ que zero representa poltrona desocupada e um 
+ representa poltrona ocupada.
 
-gaba = [0] * 10
-print("Digite o gabarito da prova")
-for i in range(10):
-    print("Questao ", i+1,":")
-    gaba[i] = int(input())
-qtdaprov = 0
-for aluno in range(15):
-    print("Aluno: ", aluno+1)
-    print("Digite o num de matricula")
-    matricula = int(input())
-    nota = 0
-    for questao in range(10):
-        print("Resp questao ", questao+1,":")
-        resp = int(input())
-        if resp == gaba[questao]:
-            nota += 1
-    print("Matricula: ", matricula)
-    print("A nota e :", nota)
-    if nota >= 6:
-        qtdaprov += 1
-perc = (qtdaprov * 100) / 15
-print("O perc de aprovados e ", perc)
+          +---+---+---+---+---+---+---+---+
+JANELA    | 0 | 1 | 0 | 0 |   | 1 | 1 | 1 |
+          +---+---+---+---+---+---+---+---+
+Poltrona    1   2   3   4  ...  22  23  24
+          +---+---+---+---+---+---+---+---+
+CORREDOR  | 0 | 1 | 0 | 0 |   | 0 | 0 | 1 |
+          +---+---+---+---+---+---+---+---+
+
+Esse programa deve controlar a venda de passagens
+da seguinte maneira:
+O cliente informa se deseja poltrona no corredor 
+ou na janela e, depois, o programa deve informar 
+quais poltronas estão disponíveis para a venda;
+	Quando não existirem poltronas livres 
+    no corredor, nas janelas ou, ainda, 
+    quando o ônibus estiver completamente 
+    cheio, deve ser mostrada uma mensagem.
+
+'''
+janela = [0] * 24
+corredor = [0] * 24
+
+opcao = 0
+while opcao != 4:
+    print("Venda de passagens")
+    print("1 - Vender passagem")
+    print("4 - Sair")
+    print("Digite a opcao desejada")
+    opcao = int(input())
+
+    if opcao == 1:
+        print("Digite janela ou corredor")
+        tipo = input()
+        if tipo == "janela":
+            for i in range(24):
+                if janela[i] == 0:
+                    print("Posicao livre: ", i+1)
+            print("Qual posicao desejada")
+            num = int(input())
+            janela[num-1] = 1
+        if tipo == "corredor":
+            for i in range(24):
+                if corredor[i] == 0:
+                    print("Posicao livre: ", i+1)
+            print("Qual posicao desejada")
+            num = int(input())
+            corredor[num-1] = 1
